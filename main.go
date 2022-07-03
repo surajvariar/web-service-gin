3866e4e81f971a7b2be9b694c2e7cec36c353a0f
surajvariar <suraj.variar@gmail.com>
2022-07-03 19:26:43 +0530

    updated code with delete method

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -27,39 +28,54 @@ func getAlbums(c *gin.Context) {
 
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
-    var newAlbum album
+	var newAlbum album
 
-    // Call BindJSON to bind the received JSON to
-    // newAlbum.
-    if err := c.BindJSON(&newAlbum); err != nil {
-        return
-    }
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
 
-    // Add the new album to the slice.
-    albums = append(albums, newAlbum)
-    c.IndentedJSON(http.StatusCreated, gin.H{"message":"Album details added successfully"})
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "Album details added successfully"})
 }
 
 // getAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
-    id := c.Param("id")
+	id := c.Param("id")
 
-    // Loop over the list of albums, looking for
-    // an album whose ID value matches the parameter.
-    for _, a := range albums {
-        if a.ID == id {
-            c.IndentedJSON(http.StatusOK, a)
-            return
-        }
-    }
-    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+//deleteAlbumByID deletes album details for the ID provided
+func deleteAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for index, album := range albums {
+		if album.ID == id {
+			albums = append(albums[:index], albums[index+1:]...)
+			c.IndentedJSON(http.StatusOK,gin.H{"message":"Album delted successfully"})
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound,gin.H{"message":"Album ID not found"})
 }
 
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-	router.GET("/albums/:id",getAlbumByID)
-	router.POST("/albums",postAlbums)
-    router.Run("localhost:8080")
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
+	router.DELETE("/albums/:id",deleteAlbumByID)
+	router.Run("localhost:8080")
 }
